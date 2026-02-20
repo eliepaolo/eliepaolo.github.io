f8355ecbc297f95ca50e55160c9e70a1758409fc
Paolo <paolo1.sani@mail.polimi.it>
2026-02-20 12:32:27 +0100

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import ComingSoon from "./pages/ComingSoon";
-import Layout from "./Layout"; // il tuo layout con navbar
-import Home from "./pages/Home";
-import Cerimonia from "./pages/Cerimonia";
-import Ricevimento from "./pages/Ricevimento";
-import Pernottamento from "./pages/Pernottamento";
-import Canti from "./pages/Canti";
-import ListaNozze from "./pages/ListaNozze";
-import RSVP from "./pages/RSVP";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Cerimonia from './pages/Cerimonia';
+import Ricevimento from './pages/Ricevimento';
+import Pernottamento from './pages/Pernottamento';
+import Canti from './pages/Canti';
+import ListaNozze from './pages/ListaNozze';
+import RSVP from './pages/RSVP';
 
 function App() {
   return (
-    <Router>
+    <>
+      <Header />
       <Routes>
-        {/* Landing */}
-        <Route path="/" element={<ComingSoon />} />
-
-        {/* Sito completo */}
-        <Route path="/info" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="cerimonia" element={<Cerimonia />} />
-          <Route path="ricevimento" element={<Ricevimento />} />
-          <Route path="pernottamento" element={<Pernottamento />} />
-          <Route path="canti" element={<Canti />} />
-          <Route path="lista-nozze" element={<ListaNozze />} />
-          <Route path="rsvp" element={<RSVP />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/cerimonia" element={<Cerimonia />} />
+        <Route path="/ricevimento" element={<Ricevimento />} />
+        <Route path="/pernottamento" element={<Pernottamento />} />
+        <Route path="/canti" element={<Canti />} />
+        <Route path="/lista-nozze" element={<ListaNozze />} />
+        <Route path="/rsvp" element={<RSVP />} />
       </Routes>
-    </Router>
+      <Footer />
+    </>
   );
 }
 
